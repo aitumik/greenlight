cad8c3feb448e0d16d775f18d2e8e570df0d55b6
aitumik <aitumik@protonmail.com>
2022-02-07 08:18:19 +0300

    feat(handler): register the handler and create a server with basic config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var version = "1.0.0"
+
+type config struct {
+	port int
+	env string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var cfg config
+
+	// Get the variables from the command line
+	flag.IntVar(&cfg.port,"port",4000,"API server port")
+	flag.StringVar(&cfg.env,"env","development","(dev|prod|staging|test)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout,"[INSIGHT]\t",log.Ldate | log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck",app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d",cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s",cfg.env,cfg.port)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
