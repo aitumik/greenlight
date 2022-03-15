12fa794a51c2444b394b4d1ddec5706a8d12eddc
aitumik <aitumik@protonmail.com>
2022-03-15 17:45:47 +0300

    feature(validator): struct for maintainging a map of errors

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,57 @@
 package validator
 
+import "regexp"
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\. [a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+// Defines new validator with a map of validation errors
 type Validator struct {
-	Name string
+	Errors map[string]string
 }
 
-func (v *Validator) Validate() {
+// New is the helper that creates the validator insance
+// with an empty errors map
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
 
+// Valid checks if the validator is valid
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+// AddError adds an error to the errors map
+func (v *Validator) AddError(key, msg string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+// Adds an error msg only if ok is true
+func (v *Validator) Check(ok bool, key, msg string) {
+	if !ok {
+		v.AddError(key, msg)
+	}
+}
+
+func In(value string, list ...string) bool {
+	for i := range list {
+		if list[i] == value {
+			return true
+		}
+	}
+	return false
+}
+
+func Matches(needle string, rx *regexp.Regexp) bool {
+	return rx.MatchString(needle)
+}
+
+func Unique(values []string) bool {
+	uniqueValues = make(map[string]bool)
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(uniqueValues) == len(values)
 }
