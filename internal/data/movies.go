c563c523de2c396c353890da2add91c34d8973e0
aitumik <aitumik@protonmail.com>
2022-03-16 16:41:45 +0300

    feat(runtime): change runtime type from int32 to Runtime

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,6 +3,7 @@ package data
 import (
 	"encoding/json"
 	"fmt"
+	"greenlight/internal/validator"
 	"time"
 )
 
@@ -11,20 +12,17 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"-"`
+	Runtime   Runtime     `json:"-"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
 
 func (m Movie) MarshalJSON() ([]byte, error) {
 	var runtime string
-
 	if m.Runtime != 0 {
 		runtime = fmt.Sprintf("%d mins", m.Runtime)
 	}
-
 	type MovieAlias Movie
-
 	// Anonymous struct
 	aux := struct {
 		MovieAlias
@@ -33,6 +31,23 @@ func (m Movie) MarshalJSON() ([]byte, error) {
 		MovieAlias: MovieAlias(m),
 		Runtime:    runtime,
 	}
-
 	return json.Marshal(aux)
+}
+
+func ValidateMovie(v *validator.Validator,movie *Movie) {
+	v.Check(movie.Title != "", "title", "title must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "title must not exceed 500 bytes")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "year must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
