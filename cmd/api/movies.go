eceb6ad3996271cd83b0c383b084d0f7b644db67
aitumik <aitumik@protonmail.com>
2022-03-18 11:32:42 +0300

    feat(handler): update movies hander

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -64,13 +64,13 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.notFoundResponse(w, r)
 		return
 	}
-	movie,err := app.models.Movies.Get(id)
+	movie, err := app.models.Movies.Get(id)
 	if err != nil {
 		switch {
-		case errors.Is(err,data.ErrRecordNotFound):
-			app.notFoundResponse(w,r)
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
 		default:
-			app.serverErrorResponse(w,r,err)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
@@ -78,5 +78,64 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		// display json formatted error to the client
 		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+	}
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// We only expect user to change these fields
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err = app.readJSON(w,r,&input)
+	if err != nil {
+		app.badRequestResponse(w,r,err)
+		return
+	}
+
+	// Copy the values from the input to the movie
+	movie.Title = input.Title
+	movie.Year  = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	v := validator.New()
+
+	if data.ValidateMovie(v,movie); !v.Valid() {
+		app.failedValidationResponse(w,r,v.Errors)
+		return
+	}
+
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
+		return
+	}
+
+	data := envelope{
+		"movie" : movie,
+	}
+	err = app.writeJSON(w,http.StatusCreated,data,nil)
+	if err != nil {
+		app.serverErrorResponse(w,r,err)
 	}
 }
