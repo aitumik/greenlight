f7f69138a513f1e97873b8d1b8fa821e63cfbf26
aitumik <aitumik@protonmail.com>
2022-08-08 13:48:23 +0300

    feat(errors): add error method for invalid authentication

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -65,3 +65,14 @@ func (app *application) rateLimiteExceededResponse(w http.ResponseWriter, r *htt
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
+
+func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
+	message := "invalid authentication credentials"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authentication", "Bearer")
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
