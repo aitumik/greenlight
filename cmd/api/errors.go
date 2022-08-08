49e8c00b3f3de3e11ab5a0594e7f3d874f593470
aitumik <aitumik@protonmail.com>
2022-08-08 18:43:50 +0300

    feat(auth): add authentication

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -76,3 +76,13 @@ func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter
 	message := "invalid or missing authentication token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must be authenticated to proceed"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you account must be activated"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
