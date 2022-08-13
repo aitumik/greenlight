0d557dae7220fddffad7d9fd0e072b3f47ec01fb
aitumik <aitumik@protonmail.com>
2022-08-13 13:07:08 +0300

    feat(models): add feature for adding permissions to user

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -52,6 +52,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// Give the user read permissions
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	// Generate the activation token
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
